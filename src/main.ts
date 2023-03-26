91a171df4e6bc7d71b69804d858dbe48d9efa051
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-03-26 13:06:05 -0300

    feat: visitNode recursive function

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,20 @@ function exampleFunction(a: number, b: number): number {
 }
 `;
 
-const tmpSourceFile = ts.createSourceFile('temp.ts', typescriptCode, ts.ScriptTarget.Latest, true);
+const tmpSourceFile = ts.createSourceFile(
+  'temp.ts',
+  typescriptCode,
+  ts.ScriptTarget.Latest,
+  true,
+);
 
-ts.forEachChild(tmpSourceFile, node => {
-    console.log(node);
-})
+/**
+ * Function to visit each node in the AST recursively
+ * @param {ts.Node} node - The node to visit
+ */
+const visitNode = (node: ts.Node) => {
+  console.log(node.kind);
+  ts.forEachChild(node, visitNode);
+};
+
+ts.forEachChild(tmpSourceFile, visitNode);
