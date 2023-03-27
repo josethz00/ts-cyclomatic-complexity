3788cad83cf4ecb8fa2e44eef124a5b104490fe0
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-03-27 12:13:52 -0300

    feat: create examples folder

---
+++ src/examples/if-else-elseif.example.ts
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param a
+ * @param b
+ * @returns
+ */
+export function exampleFunction(a: number, b: number): number {
+  if (a > b) {
+    return a;
+  } else if (a < b) {
+    return b;
+  } else {
+    return 0;
+  }
+}
