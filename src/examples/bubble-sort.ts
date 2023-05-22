2ac9b76d90c299b56bb734d42ab031a695b3f59d
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-22 19:51:49 -0300

    feat: binary search example

---
+++ src/examples/bubble-sort.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ * @param arr
+ * @returns
+ */
+export function bubbleSort(arr: number[]): number[] {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    // 1
+    for (let j = 0; j < len - i - 1; j++) {
+      // 2
+      if (arr[j] > arr[j + 1]) {
+        // 3
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
